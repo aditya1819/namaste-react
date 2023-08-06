28c6b59c721af4c95fd805085334eee3751e3654
Aditya Shahare <shahare.aditya@gmail.com>
2023-08-06 11:15:28 +0530

    Added features:
    * Search bar and to search based on entered search query
    * Filter Top hotels
    * Clear all filters and reset to all hotels
    * Fetching and filtering mock data
    * Added required states and handlers

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,14 +1,59 @@
-import React from 'react';
+import React, { useState } from 'react';
 import HotelCard from './HotelCard';
 import mock from '../utils/mock';
 
 const Body = () => {
+  const [hotelList, setHotelList] = useState(mock);
+  const [searchQuery, setSearchQuery] = useState('');
+
+  const filterHotelHandler = () => {
+    const newHotelList = hotelList.filter((item) => {
+      console.log(parseInt(item.info.avgRating));
+      return parseInt(item.info.avgRating) >= 4;
+    });
+
+    setHotelList(newHotelList);
+  };
+
+  const clearFilterHandler = () => {
+    setHotelList(mock);
+    setSearchQuery('');
+  };
+
+  const filterBySearchQuery = () => {
+    const newHotelList = mock.filter((item) => {
+      console.log(item);
+      return item.info.name.toLowerCase().includes(searchQuery.toLowerCase());
+    });
+    setHotelList(newHotelList);
+  };
+
   return (
     <div className="body">
-      <div className="search">Search Bar here</div>
+      <div className="btns">
+        <div className="search-bar">
+          <input
+            id="search-input"
+            type="text"
+            placeholder="Search..."
+            value={searchQuery}
+            onChange={(e) => setSearchQuery(e.target.value)}
+          />
+        </div>
+
+        <div className="search-btn" onClick={filterBySearchQuery}>
+          Search 🔍
+        </div>
+        <div className="filter-btn" onClick={filterHotelHandler}>
+          Top Rated ⭐
+        </div>
+        <div className="clear-filter-btn" onClick={clearFilterHandler}>
+          Clear Filter 🗙
+        </div>
+      </div>
       <div className="hotel-container">
-        {mock.map((item) => {
-          return <HotelCard data={item.info} />;
+        {hotelList.map((item) => {
+          return <HotelCard key={item.info.id} data={item.info} />;
         })}
       </div>
     </div>
