1df0134033b986ca4d7515bf89ce8e8802843dd8
Aditya Shahare <shahare.aditya@gmail.com>
2023-08-25 18:44:55 +0530

    feat: added basic routes and wip

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Header from './components/Header';
 import Body from './components/Body';
+import About from './components/About';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Error from './components/Error';
 
 const AppLayout = () => {
   return (
@@ -12,6 +15,11 @@ const AppLayout = () => {
   );
 };
 
+const appRouter = createBrowserRouter([
+  { path: '/', element: <AppLayout />, errorElement: <Error /> },
+  { path: '/about', element: <About /> }
+]);
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-root.render(<AppLayout />);
+root.render(<RouterProvider router={appRouter} />);
