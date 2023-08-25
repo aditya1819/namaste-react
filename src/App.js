e12f707f63aae64d8b2b200c308fe052f4490d88
Aditya Shahare <shahare.aditya@gmail.com>
2023-08-25 19:14:33 +0530

    React router (#3)
    * feat: added basic routes and wip
    
    * chore: children routes handling

--- src/App.js
+++ src/App.js
@@ -2,16 +2,34 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Header from './components/Header';
 import Body from './components/Body';
+import About from './components/About';
+import { createBrowserRouter, RouterProvider, Outlet } from 'react-router-dom';
+import Error from './components/Error';
 
 const AppLayout = () => {
   return (
     <div className="AppLayout">
       <Header />
-      <Body />
+
+      {/* while rendering the component corresponding to route will overrider the outlet in HTML */}
+      <Outlet />
     </div>
   );
 };
 
+const appRouter = createBrowserRouter([
+  {
+    path: '/',
+    element: <AppLayout />,
+    errorElement: <Error />,
+    // these children routes will be rendered through outlet component
+    children: [
+      { path: '/', element: <Body /> },
+      { path: '/about', element: <About /> }
+    ]
+  }
+]);
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-root.render(<AppLayout />);
+root.render(<RouterProvider router={appRouter} />);
