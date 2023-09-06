d3c61b0a484a2cb5de856d1d42dfeaa355e86078
Aditya Shahare <shahare.aditya@gmail.com>
2023-09-06 14:42:53 +0530

    chore: clear cart

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,14 +1,33 @@
 import React from 'react';
 import MenuItemList from './MenuItemList';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import { clearCart } from '../utils/store/slice/cartSlice';
 
 const Cart = () => {
   // subscribing to store using selector
   const cartItems = useSelector((store) => store.cart.items);
 
+  const dispatch = useDispatch();
+
+  const handleClearCart = () => {
+    dispatch(clearCart());
+  };
+
   return (
-    <div className="w-6/12 mx-auto shadow-lg bg-gray-50 p-4 my-4 ">
-      <MenuItemList items={cartItems}></MenuItemList>
+    <div className="text-center m-4 p-4">
+      <h1 className="text-2xl font-bold">Cart</h1>
+      <div className="w-6/12 m-auto">
+        <button
+          className=" p-2 m-2 bg-black text-white rounded-lg"
+          onClick={handleClearCart}
+        >
+          Clear Cart
+        </button>
+        {cartItems?.length === 0 && (
+          <h1> Cart is empty. Add Items to the cart!</h1>
+        )}
+        <MenuItemList items={cartItems} />
+      </div>
     </div>
   );
 };
